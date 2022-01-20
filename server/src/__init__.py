7d824346220a16bef763db5544c3ee2f8a8493c1
faunus_C <FaunusCarmel@yandex.ru>
2022-01-21 01:36:01 +0300

    re

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,13 +1,22 @@
+from dataclasses import dataclass
+
 import uvicorn
 from fastapi import FastAPI
 from starlette.responses import FileResponse
+
+
+@dataclass
+class Item:
+    name: str
+    password: str
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return Item('foo', 'bar')
 
 
 @app.get("/owner/{owner_id}")
@@ -17,8 +26,8 @@ def foo():
 
 @app.get('/favicon.ico')
 def favicon():
-    return FileResponse('favicon.ico')
+    return FileResponse('../media/favicon.ico')
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.__init__:app", host="127.0.0.1", port=5000, log_level="info", workers=4, reload=True)
+    uvicorn.run("server.src.__init__:app", host="127.0.0.1", port=5000, log_level="info", workers=4, reload=True)
